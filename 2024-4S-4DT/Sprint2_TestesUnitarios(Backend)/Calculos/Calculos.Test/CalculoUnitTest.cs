7988c80e125de6173898f7481df4e56f0a6bdc42
gabriel <gabrielhenriqueas1@hotmail.com>
2024-07-31 17:17:53 -0300

    07.31.2024

---
+++ 2024-4S-4DT/Sprint2_TestesUnitarios(Backend)/Calculos/Calculos.Test/CalculoUnitTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos.Test
+{
+    public class CalculoUnitTest
+    {
+        [Fact]
+        public void SomarDoisNumeros()
+        {
+            var n1 = 3.3;
+
+            var n2 = 2.2;
+
+            var valorEsperado = 5.5;
+
+            var soma = Calculo.Somar(n1, n2);
+
+            Assert.Equal(valorEsperado, soma);
+        }
+
+        [Fact]
+        public void SubtrairDoisNumeros()
+        {
+            var n1 = 10.0;
+
+            var n2 = 2.0;
+
+            var valorEsperado = 8.0;
+
+            var subtracao = Calculo.Subtrair(n1, n2);
+
+            Assert.Equal(valorEsperado, subtracao);
+        }
+
+        [Fact]
+        public void MultiplicarDoisNumeros()
+        {
+            var n1 = 2.0;
+
+            var n2 = 10.0;
+
+            var valorEsperado = 20.0;
+
+            var multiplicacao = Calculo.Multiplicar(n1,n2);
+
+            Assert.Equal(valorEsperado, multiplicacao);
+        }
+
+        [Fact]
+        public void DivideDoisNumeros()
+        {
+            var n1 = 10.0;
+
+            var n2 = 2.0;
+
+            var valorEsperado = 5.0;
+
+            var divisao = Calculo.Dividir(n1, n2);
+
+            Assert.Equal(valorEsperado, divisao);
+        }
+    }
+}
---
+++ 2024-4S-4DT/Sprint2_TestesUnitarios(Backend)/Calculos/Calculos/Calculo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    public static class Calculo
+    {
+        public static double Somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Subtrair(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Multiplicar(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Dividir(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
